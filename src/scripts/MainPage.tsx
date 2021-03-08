880338b123b1acc3fc14aed2c14476fcb15f0953
Nicholas Barnett <n-barnett@hotmail.com>
2021-03-07 22:25:27 -0800

    Initialize throw-away

---
+++ src/scripts/MainPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import '../styles/MainPage';
+
+export class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className='main'>Hello World</div>;
+  }
+}
