dc4d9c451f700785d034852bbe58d4d203d1b162
Nicholas Barnett <n-barnett@hotmail.com>
2021-03-07 22:25:29 -0800

    Initialize dependabot/npm_and_yarn/jquery-3.5.0

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+//main app
+import App from './scripts/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// module.hot.accept();
